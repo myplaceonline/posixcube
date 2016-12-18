7c6abbb57ab800b4452b7abfe2e8d3adac5bb026
Kevin Grigorenko <kevin@myplaceonline.com>
2016-12-17 19:52:31 -0800

    Add simple command execution

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # posixcube
+
+    usage: posixcube.sh -h HOST... [OPTION]... COMMANDS
+    posixcube.sh version ${p666_version}
+    POSIX.1-2008-standard automation scripting.
+
+      -?        Help.
+      -h HOST   Target host. May be specified multiple times.
+      -u USER   SSH user. Defaults to \${USER}.
+      -v        Show version information.
+      -d        Print debugging information.
+      -q        Quiet; minimize output.
+      -i        If using bash, install programmable tab completion for SSH hosts.
+      COMMANDS  Remote commands to run on each HOST.
+
+    Examples:
+      ./posixcube.sh -u root -h socrates -h seneca uptime
+      
+      Run the \`uptime\` command on hosts \`socrates\` and \`seneca\` as the user \`root\`.
+
+    Source: https://github.com/myplaceonline/posixcube
--- posixcube.sh
+++ posixcube.sh
@@ -11,52 +11,154 @@
 # Development guidelines:
 #   1. See references [1, 2].
 #   2. Indent with two spaces.
+#   3. Use lower-case variables unless exporting an envar [4].
 #
 # References:
 #   1. http://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html
 #   2. https://wiki.ubuntu.com/DashAsBinSh
 #   3. printf: http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap05.html
+#   4. "The name space of environment variable names containing lowercase letters is reserved for applications."
+#      http://pubs.opengroup.org/onlinepubs/009695399/basedefs/xbd_chap08.html
 #
 
-P666_VERSION=0.1
+p666_version=0.1
+p666_color_reset="\x1B[0m"
+p666_color_red="\x1B[31m"
+p666_color_green="\x1B[32m"
+
+p666_debug=0
+p666_quiet=0
+p666_hosts=""
+p666_user="${USER}"
 
 p666_show_usage () {
-  cat <<p666_show_usage_heredoc
-usage: posixcube.sh [OPTION]...
+  cat <<HEREDOC
+usage: posixcube.sh -h HOST... [OPTION]... COMMANDS
+posixcube.sh version ${p666_version}
 POSIX.1-2008-standard automation scripting.
 
-  -h, -?    Help.
+  -?        Help.
+  -h HOST   Target host. May be specified multiple times.
+  -u USER   SSH user. Defaults to \${USER}.
   -v        Show version information.
   -d        Print debugging information.
+  -q        Quiet; minimize output.
+  -i        If using bash, install programmable tab completion for SSH hosts.
+  COMMANDS  Remote commands to run on each HOST.
+
+Examples:
+  ./posixcube.sh -u root -h socrates -h seneca uptime
+  
+  Run the \`uptime\` command on hosts \`socrates\` and \`seneca\` as the user \`root\`.
 
 Source: https://github.com/myplaceonline/posixcube
-p666_show_usage_heredoc
+HEREDOC
   exit 1
 }
 
 p666_show_version () {
-  cat <<p666_show_version_heredoc
-posixcube.sh version ${P666_VERSION}
-p666_show_version_heredoc
-  exit 1
+  p666_printf "posixcube.sh version ${p666_version}\n"
+}
+
+p666_printf () {
+  p666_printf_str=$1
+  shift
+  printf "[$(date)] ${p666_printf_str}" ${*}
+}
+
+p666_printf_error () {
+  p666_printf_str=$1
+  shift
+  printf "[$(date)] ${p666_color_red}Error${p666_color_reset}: ${p666_printf_str}" ${*} 1>&2
+}
+
+p666_install () {
+  p666_func_result=0
+  if [ -d "/etc/bash_completion.d/" ]; then
+    p666_autocomplete_file=/etc/bash_completion.d/posixcube_completion.sh
+    # Autocomplete Hostnames for SSH etc.
+    # by Jean-Sebastien Morisset (http://surniaulula.com/)
+    cat <<'HEREDOC' | tee ${p666_autocomplete_file} > /dev/null
+_posixcube_complete_host () {
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  case "${prev}" in
+    \-h)
+      host_list=`{ 
+        for c in /etc/ssh_config /etc/ssh/ssh_config ~/.ssh/config
+        do [ -r $c ] && sed -n -e 's/^Host[[:space:]]//p' -e 's/^[[:space:]]*HostName[[:space:]]//p' $c
+        done
+        for k in /etc/ssh_known_hosts /etc/ssh/ssh_known_hosts ~/.ssh/known_hosts
+        do [ -r $k ] && egrep -v '^[#\[]' $k|cut -f 1 -d ' '|sed -e 's/[,:].*//g'
+        done
+        sed -n -e 's/^[0-9][0-9\.]*//p' /etc/hosts; }|tr ' ' '\n'|grep -v '*'`
+      COMPREPLY=( $(compgen -W "${host_list}" -- $cur))
+      ;;
+    *)
+      ;;
+  esac
+  return 0
+}
+complete -o default -F _posixcube_complete_host posixcube.sh
+HEREDOC
+    p666_func_result=$?
+    if [ "${p666_func_result}" = 0 ]; then
+      chmod +x ${p666_autocomplete_file}
+      p666_func_result=$?
+      if [ "${p666_func_result}" = 0 ]; then
+        source ${p666_autocomplete_file}
+        p666_func_result=$?
+        if [ "${p666_func_result}" = 0 ]; then
+          p666_printf "Installed Bash programmable completion script into ${p666_autocomplete_file}\n"
+        else
+          p666_printf "Could not execute ${p666_autocomplete_file}\n"
+        fi
+      else
+        p666_printf "Could not chmod +x ${p666_autocomplete_file}\n"
+      fi
+    else
+      p666_printf "Could not create ${p666_autocomplete_file}\n"
+      p666_printf "You may need to try with sudo. For example:\n"
+      p666_printf "  sudo ./posixcube.sh -i && source ${p666_autocomplete_file}\n"
+      p666_printf "You only need to source the command the first time. Subsequent shells will automatically source it.\n"
+    fi
+  else
+    p666_printf "No directory /etc/bash_completion.d/ found, skipping Bash programmable completion installation.\n"
+  fi
+  exit ${p666_func_result}
 }
 
 # getopts processing based on http://stackoverflow.com/a/14203146/5657303
 OPTIND=1 # Reset in case getopts has been used previously in the shell.
 
-# Initialize our own variables:
-p666_debug=0
-
-while getopts "h?vd" p666_opt; do
+while getopts "?vdqih:u:" p666_opt; do
   case "$p666_opt" in
-  h|\?)
+  \?)
     p666_show_usage
     ;;
   v)
     p666_show_version
+    exit 1
     ;;
   d)
     p666_debug=1
+    ;;
+  q)
+    p666_quiet=1
+    ;;
+  i)
+    p666_install
+    ;;
+  h)
+    if [ "${p666_hosts}" = "" ]; then
+      p666_hosts="${OPTARG}"
+    else
+      p666_hosts="${p666_hosts} ${OPTARG}"
+    fi
+    ;;
+  u)
+    p666_user="${OPTARG}"
     ;;
   esac
 done
@@ -65,4 +167,26 @@ shift $((OPTIND-1))
 
 [ "$1" = "--" ] && shift
 
-[ "${p666_debug}" = 1 ] && printf "debug=%d, leftovers: %s\n" ${p666_debug} ${@}
+p666_commands="${@}"
+
+[ "${p666_debug}" = 1 ] && p666_printf "debug=${p666_debug}, leftovers: ${p666_commands}\n"
+
+if [ "${p666_hosts}" = "" ]; then
+  p666_printf_error "No hosts specified with -h.\n\n" 1>&2
+  p666_show_usage
+fi
+
+if [ "${p666_commands}" = "" ]; then
+  p666_printf_error "No COMMANDS specified.\n\n" 1>&2
+  p666_show_usage
+fi
+
+[ "${p666_quiet}" = 0 ] && p666_show_version
+
+for p666_host in ${p666_hosts}; do
+  p666_printf "[${p666_color_green}${p666_host}${p666_color_reset}]: Executing ssh ${p666_user}@${p666_host} ${p666_commands}...\n"
+  p666_host_output=$(ssh ${p666_user}@${p666_host} ${p666_commands})
+  p666_printf "[${p666_color_green}${p666_host}${p666_color_reset}]: ${p666_host_output}\n"
+done
+
+exit $?
