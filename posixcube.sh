d7d1e30cfbb5d7bffbce480df32bc622245bfb21
Kevin Grigorenko <kevin@myplaceonline.com>
2016-12-17 17:00:15 -0800

    Initial version of posixcube.sh

---
+++ posixcube.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# posixcube.sh: POSIX.1-2008 standard scripts for automation.
+#
+# Authors:
+#   Kevin Grigorenko (kevin@myplaceonline.com)
+#
+# Version History:
+#   0.1
+#     * Version 0.1
+#
+# Development guidelines:
+#   1. See references [1, 2].
+#   2. Indent with two spaces.
+#
+# References:
+#   1. http://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html
+#   2. https://wiki.ubuntu.com/DashAsBinSh
+#
